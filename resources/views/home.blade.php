506a3889c38c8c9612f511bfccdf795e13f1663c
Muhammad Zaky <jackdesign03@gmail.com>
2025-03-29 23:33:44 +0700

    struktur awal website yayasan

---
+++ resources/views/home.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <title>Yayasan Al Alawiyah</title>
+</head>
+<body>
+    <h1>Selamat Datang di Yayasan Al Alawiyah</h1>
+    <p>Website ini sedang dalam pengembangan.</p>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
+<<<<<<< HEAD
 <?php
 
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+=======
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('home');
+>>>>>>> 710974e (struktur awal website yayasan)
 });
